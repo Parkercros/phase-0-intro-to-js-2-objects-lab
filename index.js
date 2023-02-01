c7e2cd39d823ab825631125fbfd5bea63e31a93a
Parker Crosgrove <pdxparker3@gmail.com>
2023-02-01 11:19:51 -0700

    fiinished lab

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-// initialize employee object
-const employee = { name: 'John Doe', streetAddress: '123 Main St' };
 
-// updateEmployeeWithKeyAndValue() function
+const employee = {
+    name: "Sam",
+    streetAddress: "123 Main St",
+
+}
+
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return { ...employee, [key]: value };
-}
+    const newObject = {...employee}
+    newObject[key] = value;
+  
+    return newObject;
+  }
 
-// destructivelyUpdateEmployeeWithKeyAndValue() function
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+ {
     employee[key] = value;
+
     return employee;
-}
+ }
 
-// deleteFromEmployeeByKey() function
-function deleteFromEmployeeByKey(employee, key) {
-    const { [key]: deletedKey, ...updatedEmployee } = employee;
-    return updatedEmployee;
-}
+ function deleteFromEmployeeByKey(employee, key){
+    const newObject = {employee} 
+    delete employee.key
 
-// destructivelyDeleteFromEmployeeByKey() function
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    return newObject;
+    
+    
+ }
+ 
+ function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
     return employee;
-}
 
-module.exports = {
-    updateEmployeeWithKeyAndValue,
-    destructivelyUpdateEmployeeWithKeyAndValue,
-    deleteFromEmployeeByKey,
-    destructivelyDeleteFromEmployeeByKey,
-}
-
+ }
 
